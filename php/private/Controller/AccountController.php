91f00b4c51d8fe7dc5fb0ae2d9222a769d541fdf
Nick Hartjes <info@nickhartjes.nl>
2018-06-16 10:12:46 +0200

    Added logout page

--- php/private/Controller/AccountController.php
+++ php/private/Controller/AccountController.php
@@ -1,110 +1,132 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: NickHartjes
- * Date: 09/06/2018
- * Time: 10:37
- */
-
 namespace Webshop\Controller;
 
 use Webshop\Core\Controller;
 use Webshop\Core\Util;
-use Webshop\Model\User;
 
 class AccountController extends Controller
 {
 
+    private $loginError = "loginError";
+    private $loginForm = "loginForm";
+    private $registerError = "registerError";
+    private $registerForm = "registerForm";
+    private $userSession = "user";
     /**
      * @all controllers must contain an index method
      */
     function index()
     {
-        $this->registry->template->title = "Nick";
-        $this->registry->template->description = "Fout";
+        $this->registry->template->title = "Account";
+        $this->registry->template->description = "Account pagina";
         $this->registry->template->cartItems = Util::getNrCartItems();
+        $this->registry->template->loginSend = Util::checkExcistInSession($this->loginError);
+        $this->registry->template->registerSend = Util::checkExcistInSession($this->registerError);
         $this->registry->template->show('account');
     }
 
     function login()
     {
         if (isset($_POST['loginSubmit'])) {
+            // Unset previous values
+            unset($_SESSION[$this->loginError]);
+            unset($_SESSION[$this->loginForm]);
+
             $user = new \Webshop\Model\User();
 
-            $formulier      = $_POST['login'];
-            $emailadres     = $formulier['emailadres'];
-            $password       = $formulier['password'];
+            $form = $_POST['login'];
+            $emailadres = $form['emailadres'];
+            $password = $form['password'];
 
             $loggedInUser = $user->getOne('emailadres', $emailadres);
-            if(is_object($loggedInUser)){
-                $password =  password_verify($password, $loggedInUser->password);
-                if($password) {
-                    $_SESSION['user'] = $loggedInUser;
-                }else {
-                    $_SESSION['loginFormError'] = "Your email or password is incorrect";
+            if (is_object($loggedInUser)) {
+                $password = password_verify($password, $loggedInUser->password);
+                if ($password) {
+                    $_SESSION[$this->userSession] = $loggedInUser;
+                    header('Location: /');
+                    die();
                 }
-            } else {
-                $_SESSION['loginFormError'] = "Your email or password is incorrect";
             }
-            var_dump($_SESSION);
+
+            // Save errors in a session
+            $_SESSION[$this->loginForm] = $form;
+            Util::redirectWithError('/account', $this->loginError, "Your email or password is incorrect");
         }
     }
 
     function register()
     {
-        echo '<pre>';
-        var_dump($_POST);
-        echo '</pre>';
         if (isset($_POST['registerSubmit'])) {
-            $user = new \Webshop\Model\User();
+            // Unset previous values
+            unset($_SESSION[$this->registerError]);
+            unset($_SESSION[$this->registerForm]);
 
+            $user = new \Webshop\Model\User();
 
             $errorCatched = false;
 
-            $form           = $_POST['register'];
-            $emailadres     = (string) $form['emailadres'];
-            $passwordFirst  = (string) $form['passwordFirst'];
-            $passwordSecond = (string) $form['passwordSecond'];
-            $firstName      = (string) $form['firstName'];
-            $lastName       = (string) $form['lastName'];
-            $dayOfBirth     = \DateTime::createFromFormat('Y-m-d', $form['dayOfBirth']); //date('Y-m-d', $form['dayOfBirth']);
-            $sex            = (string) $form['sex'];
+            $form = $_POST['register'];
+            $emailadres = (string)$form['emailadres'];
+            $passwordFirst = (string)$form['passwordFirst'];
+            $passwordSecond = (string)$form['passwordSecond'];
+            $firstName = (string)$form['firstName'];
+            $lastName = (string)$form['lastName'];
+            $dayOfBirth = \DateTime::createFromFormat('Y-m-d', $form['dayOfBirth']);
+            $sex = (string)$form['sex'];
 
-            foreach ($form as $name => $value){
-                if(empty($value)){
-                    $_SESSION['registerFormError']['blank'] = '<p>Some Fields were left blank. Please fill up all fields.</p>';
+            foreach ($form as $name => $value) {
+                if (empty($value)) {
+                    $_SESSION[$this->registerError]['blank'] = 'Some Fields were left blank. Please fill up all fields.';
                     $errorCatched = true;
                 }
             }
 
-            switch (true){
+            switch (true) {
                 case !filter_var($emailadres, FILTER_VALIDATE_EMAIL):
-                    $_SESSION['registerFormError']['email'] = '<p>The emailadres you gave is not valid.</p>';
+                    $_SESSION[$this->registerError]['email'] = 'The emailadres you gave is not valid.';
                     $errorCatched = true;
                     break;
                 case ($passwordFirst != $passwordSecond):
-                    $_SESSION['registerFormError']['password'] = '<p>The Passwords you entered didn\'t match</p>';
+                    $_SESSION[$this->registerError]['password'] = 'The Passwords you entered didn\'t match';
                     $errorCatched = true;
                     break;
                 case !is_bool($user->getOne('emailadres', $emailadres)):
-                    $_SESSION['registerFormError']['registered'] = '<p>This email is already registred</p>';
+                    $_SESSION[$this->registerError]['registered'] = 'This email is already registred';
                     $errorCatched = true;
                     break;
             }
 
-            if(!$errorCatched){
+            if (!$errorCatched) {
                 $user = new \Webshop\Model\User();
 
-                $user->emailadres   = $emailadres;
-                $user->password     = $passwordFirst;
-                $user->firstName    = $firstName;
-                $user->lastName     = $lastName;
-                $user->dayOfBirth   = $dayOfBirth;
-                $user->sex          = $sex;
+                $user->emailadres = $emailadres;
+                $user->password = $passwordFirst;
+                $user->firstName = $firstName;
+                $user->lastName = $lastName;
+                $user->dayOfBirth = $dayOfBirth;
+                $user->sex = $sex;
                 var_dump($emailadres);
                 var_dump($user);
                 $user->save();
+
+//                header('Location: /');
+            } else {
+                // There are errors, so redirect back to the account page and save the errors in a session
+                $_SESSION[$this->registerForm] = $form;
+                header('Location: /account');
             }
         }
+    }
+
+    function logout(){
+        unset($_SESSION[$this->userSession]);
+        unset($_SESSION[$this->registerError]);
+        unset($_SESSION[$this->registerForm]);
+        unset($_SESSION[$this->loginError]);
+        unset($_SESSION[$this->loginForm]);
+        $this->registry->template->title = "Account";
+        $this->registry->template->description = "Account pagina";
+        $this->registry->template->cartItems = Util::getNrCartItems();
+        $this->registry->template->show('logout');
     }
 }
--- php/private/Core/Util.php
+++ php/private/Core/Util.php
@@ -12,7 +12,7 @@ class Util
         return preg_replace('/-+/', '', $text); // Replaces multiple hyphens with single one.
     }
 
-    static function cleanStringAndTruncate( $text, $length)
+    static function cleanStringAndTruncate($text, $length)
     {
         $text = (strlen($text) > 150) ? substr($text, 0, 200) . '...' : $text;
         // Remove all headings and text
@@ -21,15 +21,26 @@ class Util
         return strip_tags($text);
     }
 
-    static function deleteElement($element, &$array){
+    static function redirectWithError($page, $errorKey, $errorValue, $unsetUser = false)
+    {
+        $_SESSION[$errorKey] = $errorValue;
+        if ($unsetUser) {
+            unset($_SESSION[user]);
+        }
+        header('Location: ' . $page);
+    }
+
+    static function deleteElement($element, &$array)
+    {
         $index = array_search($element, $array);
-        if($index !== false){
+        if ($index !== false) {
             unset($array[$index]);
         }
     }
 
-    static function getNrCartItems() {
-        if(isset($_SESSION['cart']))
+    static function getNrCartItems()
+    {
+        if (isset($_SESSION['cart']))
             $nrCartItems = count($_SESSION['cart']);
         else {
             $nrCartItems = 0;
@@ -37,4 +48,13 @@ class Util
         return $nrCartItems;
     }
 
+    static function checkExcistInSession($variabele)
+    {
+        if (array_key_exists ($variabele, $_SESSION)) {
+            if (isset($variabele) && !empty($variabele)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
